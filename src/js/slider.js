b5a06cc37337c7a0fb911d4a75467fb268c11275
AmetDev <ametsejdaliev@gmail.com>
2024-03-05 23:13:53 +0300

    need-to-fix-slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,17 @@
 import airJordan from '../images/airjordan-background.png'
 import imgFrog from '../images/imgFrog.png'
+import vector from '../images/vector.svg'
 import { Slider } from './models/SliderClass'
-
 export const sliderFunc = () => {
   let counter = 0
   const ImgSlider = document.createElement('img')
   const title = document.createElement('span')
   const btn = document.createElement('button')
   const btnSwitch = document.querySelector('#btnSwitch')
+
+  const imgInBtn = document.createElement('img')
+  imgInBtn.src = vector
+  btnSwitch.appendChild(imgInBtn)
   const wrapper_slider_element = document.createElement('div')
   const wrapper_slider = document.querySelector('#wrapper_slider')
   const wrapperBtnSlider = document.querySelector('#wrapperBtnSlider')
@@ -31,19 +35,24 @@ export const sliderFunc = () => {
   )
 
   ImgSlider.src = elements[counter].image
+  title.textContent = elements[counter].title
+
+  title.classList = 'titleImageSlider'
   wrapper_slider_element.appendChild(ImgSlider)
+  wrapper_slider_element.appendChild(title)
+
   wrapper_slider.appendChild(wrapper_slider_element)
 
   btnSwitch.addEventListener('click', () => {
     counter += 1
     if (counter == elements.length) {
-      console.log('counter', counter)
-      console.log('elements.length', elements.length)
-      btnSwitch.setAttribute('disabled', 'disabled')
+      counter = 0
     }
     wrapper_slider_element.removeChild(ImgSlider)
     ImgSlider.src = elements[counter].image
+    title.textContent = elements[counter].title
     wrapper_slider_element.appendChild(ImgSlider)
+    wrapper_slider_element.appendChild(title)
     wrapper_slider.appendChild(wrapper_slider_element)
   })
 
