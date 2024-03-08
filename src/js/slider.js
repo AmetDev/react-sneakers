b64d5b0b0313bac414413e33bc37d902c0555970
AmetDev <ametsejdaliev@gmail.com>
2024-03-08 17:27:15 +0300

    slider-done

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,14 +4,16 @@ import vector from '../images/vector.svg'
 import { Slider } from './models/SliderClass'
 export const sliderFunc = () => {
   let counter = 0
-  const ImgSlider = document.createElement('img')
+  const ImgSlider = document.createElement('div')
   const title = document.createElement('span')
   const btn = document.createElement('button')
-  const btnSwitch = document.querySelector('#btnSwitch')
+  const btnSwitch = document.createElement('button')
 
   const imgInBtn = document.createElement('img')
+
   imgInBtn.src = vector
   btnSwitch.appendChild(imgInBtn)
+  btnSwitch.setAttribute('id', 'btnSwitch')
   const wrapper_slider_element = document.createElement('div')
   const wrapper_slider = document.querySelector('#wrapper_slider')
   const wrapperBtnSlider = document.querySelector('#wrapperBtnSlider')
@@ -19,11 +21,13 @@ export const sliderFunc = () => {
     {
       image: imgFrog,
       button_text: 'КУПИТЬ',
+      href_btn: '#',
       title: 'Stan Smith, Forever!',
     },
     {
       image: airJordan,
       button_text: 'ПОЛУЧИТЬ СКИДКУ',
+      href_btn: '#',
       title: 'Air Jordan, Forever!',
     },
   ]
@@ -33,13 +37,20 @@ export const sliderFunc = () => {
     elements[counter].button_text,
     elements[counter].title
   )
+  ImgSlider.appendChild(btnSwitch)
+  btn.textContent = elements[counter].button_text
+  ImgSlider.appendChild(btn)
+  btn.classList = 'SliderBtn'
+  ImgSlider.style.background = `no-repeat url(${elements[counter].image})`
+  ImgSlider.classList = 'ImgSlider'
+  ImgSlider.style.width = '960px'
+  ImgSlider.style.height = '300px'
 
-  ImgSlider.src = elements[counter].image
   title.textContent = elements[counter].title
 
   title.classList = 'titleImageSlider'
   wrapper_slider_element.appendChild(ImgSlider)
-  wrapper_slider_element.appendChild(title)
+  ImgSlider.appendChild(title)
 
   wrapper_slider.appendChild(wrapper_slider_element)
 
@@ -49,10 +60,23 @@ export const sliderFunc = () => {
       counter = 0
     }
     wrapper_slider_element.removeChild(ImgSlider)
-    ImgSlider.src = elements[counter].image
+    ImgSlider.removeChild(btn)
+    ImgSlider.removeChild(title)
+    ImgSlider.appendChild(btnSwitch)
+    btn.textContent = elements[counter].button_text
+    ImgSlider.appendChild(btn)
+    btn.classList = 'SliderBtn'
+    ImgSlider.style.background = `no-repeat url(${elements[counter].image})`
+    ImgSlider.classList = 'ImgSlider'
+    ImgSlider.style.width = '960px'
+    ImgSlider.style.height = '300px'
+
     title.textContent = elements[counter].title
+
+    title.classList = 'titleImageSlider'
     wrapper_slider_element.appendChild(ImgSlider)
-    wrapper_slider_element.appendChild(title)
+    ImgSlider.appendChild(title)
+
     wrapper_slider.appendChild(wrapper_slider_element)
   })
 
