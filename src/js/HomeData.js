ab8d9bb5a100357e5357d06ffc9c75b24d376c45
AmetDev <ametsejdaliev@gmail.com>
2024-03-09 17:39:55 +0300

    add-grid-sneakers

--- src/js/HomeData.js
+++ src/js/HomeData.js
@@ -1,5 +1,9 @@
+import done from '../images/bi_check-lg.svg'
+import like from '../images/like2.svg'
+import liked from '../images/liked.svg'
 import plus from '../images/plus.svg'
 import { ElementGridSneakers } from './models/GridClass'
+
 var dataSneakers = []
 
 const fetchedData = async () => {
@@ -14,11 +18,10 @@ const fetchedData = async () => {
 }
 
 export const HomeGridSneakersRender = () => {
-  console.log(dataSneakers)
   const wrapperGridSneakers = document.querySelector('#gridSneakers')
   dataSneakers.forEach((element) => {
     const object = new ElementGridSneakers(element.url, element.liked, element.text, element.price)
-    console.log('it a object', object)
+
     const imgSneakers = document.createElement('img')
     const textSneakers = document.createElement('span')
     const priceSneakers = document.createElement('span')
@@ -27,6 +30,13 @@ export const HomeGridSneakersRender = () => {
     const nameSneakers = document.createElement('div')
     const buttonSneakers = document.createElement('div')
     const plusButton = document.createElement('img')
+    const textPrice = document.createElement('span')
+    const blockPriceWrapper = document.createElement('div')
+    const likeElement = document.createElement('img')
+    const likedElement = document.createElement('img')
+    const likeBtn = document.createElement('button')
+    likeElement.src = like
+    likedElement.src = liked
     imgSneakers.src = object.img
     imgSneakers.style.width = '133px'
     imgSneakers.style.height = '112px'
@@ -34,20 +44,65 @@ export const HomeGridSneakersRender = () => {
     textSneakers.classList = 'textSneakers'
     buttonSneakers.classList = 'buttonSneakers'
     //buttonSneakers.textContent = '+'
-    plusButton.style.background = `no-repeat url(${plus})`
+    plusButton.src = plus
     plusButton.style.width = '11px'
     plusButton.style.height = '11px'
-
+    plusButton.classList = 'plusButton'
+    textPrice.textContent = 'ЦЕНА:'
+    textPrice.classList = 'textPrice'
     priceSneakers.textContent = `${object.price} руб.`
     buttonSneakers.appendChild(plusButton)
     priceBlock.classList = 'priceSneakers'
+    likeBtn.appendChild(likeElement)
+    wrapperSneakersElement.appendChild(likeBtn)
     wrapperSneakersElement.appendChild(imgSneakers)
+    wrapperSneakersElement.classList = 'wrapperSneakersElement'
+    likeBtn.classList = 'likeBtn'
     nameSneakers.appendChild(textSneakers)
-    priceBlock.appendChild(priceSneakers)
+    blockPriceWrapper.appendChild(textPrice)
+    blockPriceWrapper.appendChild(priceSneakers)
+    blockPriceWrapper.classList = 'blockPriceWrapper'
+    priceBlock.appendChild(blockPriceWrapper)
     priceBlock.appendChild(buttonSneakers)
     wrapperSneakersElement.appendChild(nameSneakers)
     wrapperSneakersElement.appendChild(priceBlock)
     wrapperGridSneakers.appendChild(wrapperSneakersElement)
+
+    let checker = true
+    plusButton.addEventListener('click', () => {
+      if (checker == false) {
+        plusButton.src = plus
+        plusButton.classList.remove('--active')
+        wrapperSneakersElement.classList.remove('--active-element')
+        element.selected = false
+
+        checker = true
+      } else {
+        plusButton.src = done
+        plusButton.classList.add('--active')
+        wrapperSneakersElement.classList.add('--active-element')
+        element.selected = true
+
+        checker = false
+      }
+    })
+    let checkerLike = true
+    likeBtn.addEventListener('click', () => {
+      if (checkerLike == false) {
+        likeElement.src = like
+        element.liked = false
+        localStorage.removeItem('object', JSON.stringify(element))
+        likeBtn.classList.remove('--active-like')
+        checkerLike = true
+      } else {
+        likeElement.src = liked
+        likeBtn.classList.add('--active-like')
+
+        element.liked = true
+        localStorage.setItem('object', JSON.stringify(element))
+        checkerLike = false
+      }
+    })
   })
 }
 
